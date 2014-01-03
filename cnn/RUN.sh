9394a0bce8d0e20ccecc85ff15f4700a708be13c
zhangce <czhang@cs.wisc.edu>
2014-01-03 13:01:17 -0800

    a

--- cnn/RUN.sh
+++
@@ -1,5 +0,0 @@
-
-cd paleodeepdive.sampler
-
-sbt "run-main org.hazy.gibbs.GibbsSampler -v ../variables.txt -f ../factors.txt -w ../weights.txt -l 10 -s 10 -i 10 -t 4 -o /tmp/out.out --alpha 0.01"
-
